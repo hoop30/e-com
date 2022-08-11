aa12bc48f2456ce93f52c2c641b341658e237bd5
hoop30 <kevin.granier@gmail.com>
2022-08-11 08:36:00 +0200

    #4 Header
    Header: mobile integration finished

--- src/components/header/HeaderMenu.jsx
+++ src/components/header/HeaderMenu.jsx
@@ -5,20 +5,28 @@ export function HeaderMenu() {
     return (
         <ul className='headerMenu'>
             <li>
-                <a aria-current="page" href="index.css">Produit</a>
-                <BsThreeDots size="2em" color='#ffd1b3'/>
+                <a aria-current="page" href="index.css">
+                    Produit
+                    <BsThreeDots size="2em" color='#ffd1b3' />
+                </a>
             </li>
             <li>
-                <a href="index.css">Promotions</a>
-                <BsThreeDots size="2em" color='#ffd1b3'/>
+                <a href="index.css">
+                    Promotions
+                    <BsThreeDots size="2em" color='#ffd1b3' />
+                </a>
             </li>
             <li>
-                <a href="index.css">Contact</a>
-                <BsThreeDots size="2em" color='#ffd1b3'/>
+                <a href="index.css">
+                    Contact
+                    <BsThreeDots size="2em" color='#ffd1b3' />
+                </a>
             </li>
             <li>
-                <a href="index.css">Aide</a>
-                <BsThreeDots size="2em" color='#ffd1b3'/>
+                <a href="index.css">
+                    Aide
+                    <BsThreeDots size="2em" color='#ffd1b3' />
+                </a>
             </li>
         </ul>
     )
