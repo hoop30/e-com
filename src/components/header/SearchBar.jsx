c543bc30118546ccf760e5e816184c0e942dcf58
hoop30 <kevin.granier@gmail.com>
2022-08-14 00:13:56 +0200

    10# Header
    Header: header sticky on scroll responsive

--- src/components/header/SearchBar.jsx
+++ src/components/header/SearchBar.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import { IoSearchOutline } from "react-icons/io5";
+import React, { useState } from 'react'
+import { IoSearchOutline } from "react-icons/io5"
+import { ScrollListener } from '../../utils/ScrollListener'
 
 export function SearchBar() {
     
+    const [search, setSearch] = useState('search')
+
+    ScrollListener(setSearch, '', 'search searchSticky')
+
     return (
-        <form role="search">
+        <form className={search}>
             <input className="searchForm" type="search" aria-label="Search" placeholder="Rechercher" />
             <button className='searchBtn'>
                 <IoSearchOutline size="3em" color='grey' id='searchBtn' />
